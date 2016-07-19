6969b76158635ebe1e8375727d28163604932540
DC <dan212@gmail.com>
2016-07-19 13:20:57 -0400

    Fix

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,4 +8,4 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
-exec /tmp/openresty/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
+exec $HOME/vendor/openresty/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
