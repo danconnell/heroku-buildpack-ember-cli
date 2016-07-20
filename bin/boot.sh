de31b6c17338290e398627144d1e1e72cb394c74
DC <dan212@gmail.com>
2016-07-20 16:31:28 -0400

    Use the new build zip

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,6 +8,5 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
-build_path=$(ls $HOME/vendor/openresty-1.9.15.1/build/luajit-root/tmp | head -n 1)
-export LD_LIBRARY_PATH=$HOME/vendor/openresty-1.9.15.1/build/luajit-root/tmp/$build_path/vendor/openresty/luajit/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/vendor/openresty-1.9.15.1/build/luajit-root/tmp/app/vendor/openresty-heroku-build/luajit/lib:$LD_LIBRARY_PATH
 exec $HOME/vendor/openresty/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
