3abfd928bc2c8d5a222332574fe6892dc3b6b4a3
DC <dan212@gmail.com>
2021-02-01 13:37:59 -0500

    Update openresty

--- bin/boot.sh
+++ bin/boot.sh
@@ -8,5 +8,5 @@ touch $HOME/logs/nginx/access.log $HOME/logs/nginx/error.log
 
 (tail -f -n 0 $HOME/logs/nginx/*.log &)
 
-export LD_LIBRARY_PATH=$HOME/vendor/openresty-1.9.15.1/build/luajit-root/app/vendor/openresty-heroku-build/luajit/lib:$LD_LIBRARY_PATH
-exec $HOME/vendor/openresty-heroku-build/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
+export LD_LIBRARY_PATH=$HOME/vendor/openresty/luajit/lib:$LD_LIBRARY_PATH
+exec $HOME/vendor/openresty/nginx/sbin/nginx -p $HOME -c $HOME/config/nginx.conf
